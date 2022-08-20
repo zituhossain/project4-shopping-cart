111e2a01961c628d1bfa34ed2bd3957724debcc8
zituhossain <zituhossain@gmail.com>
2022-08-21 01:56:15 +0600

    create filter component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { CartState } from "../context/Context";
+import Filters from "./Filters";
+import SingleProduct from "./SingleProduct";
+import "./style.css"
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const {
+    state: { products },
+  } = CartState();
 
-export default Home
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProduct prod={prod} key={prod.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/SingleProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleProduct = ({prod}) => {
+  return (
+    <div>{prod.name}</div>
+  )
+}
+
+export default SingleProduct
