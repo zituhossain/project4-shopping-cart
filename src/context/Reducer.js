9f538d726bc10881374d3a466e063aaa1be501aa
zituhossain <zituhossain@gmail.com>
2022-08-22 12:31:45 +0600

    change cart quantity functionality

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -11,11 +11,18 @@ export const cartReducer = (state, action) => {
           },
         ],
       };
-      case "ROMOVE_FROM_CART":
-        return {
-          ...state,
-          cart: state.cart.filter((c)=>c.id !==action.payload.id)
-        }
+    case "ROMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+    case "CHANGE_CART_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.filter((c) =>
+          c.id === action.payload.id ? (c.qty = action.payload.qty) : c.qty
+        ),
+      };
     default:
       return state;
   }
